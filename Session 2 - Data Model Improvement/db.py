ed1b715cff8ef0373dfd7e9eca1345e38ebd6ef6
NinoDulay <altisjessienino18@gmail.com>
2024-09-16 10:04:53 +0800

    Added instructions for session 2

--- Session 2 - Data Model Improvement/db.py
+++ Session 2 - Data Model Improvement/db.py
@@ -5,5 +5,30 @@ items = {}
 # 'HTTP GET REQUEST localhost/item'
 # print(list(items.values()))
 
+'''
+    stores = {
+        "id": "w939r4y9mdf390"
+        "name": "Nino Dulay"
+    }
+
+'''
 
 
+
+'''
+    items = {
+        1: {
+            "name": "Chair",
+            "price": 17.99,
+            "store_id": "w939r4y9mdf390"
+        },
+        2: {
+            "name": "Laptop",
+            "price": 17.99,
+            "store_id": "w939r4y9mdf390"
+        }
+    }
+
+    # We do not have to use for loop just to find a specific item
+    items[1]
+'''
